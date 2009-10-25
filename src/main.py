68ed222a453f6c59da587241b65ddd47b4508f21
Joshua Poehls <joshua@zduck.com>
2009-10-25 10:52:27 -0500

    got login and logout links working

--- src/main.py
+++ src/main.py
@@ -4,27 +4,36 @@ from google.appengine.api import users
 from Cheetah.Template import Template
 
 urls = (
-        "/(.*)", "pages"
+        "/(.*)", "default"
 )
 
 template_folder = "pages/"
 
 app = web.application(urls, globals()) 
 
-class pages:      
+class login:
+    def GET(self):
+        web.redirect(users.createLoginUrl())
+        return None
+
+class default:
     def GET(self, path=None):
         
         user = users.get_current_user()
         
+        vars = {}
+
         if user:
             path = "me"
+            vars['logoutUrl'] = users.create_logout_url("/")
+            vars['nickname'] = user.nickname();
         else:
             path = "index"    
+            vars['loginUrl'] = users.create_login_url("/")
                   
         tmpl_file = template_folder + path + ".htm"
         if os.path.exists(tmpl_file):
-            template_values = { "xyz":"You",}
-            tmpl = Template( file = tmpl_file, searchList = (template_values,) )
+            tmpl = Template( file = tmpl_file, searchList = (vars,) )
             return tmpl
         else:
             return "404: Page not found"
